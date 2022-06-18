d2fcf9a60937211e703ad6cdb0f2b3dad49a5361
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-06-18 23:04:22 +0200

    Use About component for Restaurant

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,16 @@ console.log('Home')
 </script>
 
 <template>
-  <main></main>
+  <main>
+    <h1>
+      Grab your
+      <br />
+      <strong>delicious meal</strong>
+    </h1>
+    <input type="search" name="restaurantType" id="restaurantType" />
+    <br /><br />
+    Restaurant type selector <br />
+    Restaurants: <br />
+    Restaurants list <br />
+  </main>
 </template>
