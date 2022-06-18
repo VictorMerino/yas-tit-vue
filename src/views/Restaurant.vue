b7adbb951f46f365a842de7e05caf476476256b8
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-06-18 22:39:51 +0200

    Add Restaurant view: to be filled

---
+++ src/views/Restaurant.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="restaurant">
+    <h1>This is the restaurant page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .restaurant {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
