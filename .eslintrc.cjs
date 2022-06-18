edc5253e726923799bd44c1a5fa3efa2fe2f2cb1
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-06-18 15:05:22 +0200

    Prettier just doing its thing

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,32 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
   extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript/recommended",
-    "@vue/eslint-config-prettier",
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    '@vue/eslint-config-prettier',
   ],
   overrides: [
     {
-      files: ["cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}"],
-      extends: ["plugin:cypress/recommended"],
+      files: ['cypress/e2e/**.{cy,spec}.{js,ts,jsx,tsx}'],
+      extends: ['plugin:cypress/recommended'],
     },
   ],
   rules: {
-    "prettier/prettier": [
-      "warn",
+    'prettier/prettier': [
+      'warn',
       {
         semi: false,
         singleQuote: true,
-        arrowParens: "avoid",
+        arrowParens: 'avoid',
         printWidth: 80,
-        trailingComma: "none",
+        trailingComma: 'none',
         bracketSameLine: true,
         // "breakBeforeElse": true
       },
     ],
   },
-};
+}
