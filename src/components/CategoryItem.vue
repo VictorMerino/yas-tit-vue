e33fc6f09205206f572f6de0b2758a35365bb907
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-06-19 22:35:57 +0200

    Categories buttons

--- src/components/CategoryItem.vue
+++ src/components/CategoryItem.vue
@@ -1,6 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { Item } from '@/types'
+
+defineProps<{
+  category: Item
+}>()
+</script>
 
 <template>
-  <div></div>
+  <div
+    style="
+      background-color: #fff;
+      border-radius: 50px;
+      padding: 1.5em 1em;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+    ">
+    <img :src="category.image" style="width: 50px; height: 50px" />
+    <p>{{ category.name }}</p>
+  </div>
 </template>
 <style scoped></style>
--- src/components/RestaurantCategories.vue
+++ src/components/RestaurantCategories.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
-import type { Category, Item } from '@/types'
+import type { Item } from '@/types'
+import CategoryItem from './CategoryItem.vue'
 // setCategory: Function
 
 defineProps<{
-  categories: Category[]
+  categories: Item[]
   activeItem: Item
 }>()
 </script>
 <template>
+  <div class="categories-wrapper">
+    <CategoryItem
+      v-for="category in categories"
+      :key="category.name"
+      :category="category" />
+  </div>
+
   <div>Active Item: {{ activeItem }}</div>
 </template>
+
+<style>
+.categories-wrapper {
+  display: flex;
+  gap: 2em;
+}
+</style>
