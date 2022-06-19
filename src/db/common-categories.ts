421ae6f11d2cdcdba606951e0666230d8f751130
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-06-19 16:15:14 +0200

    Add commonCategories constant from external file

---
+++ src/db/common-categories.ts
@@ -0,0 +1,27 @@
+import type { Item } from '@/types'
+export const commonCategories: Item[] = [
+  {
+    name: 'burger',
+    image: `/images/burger.png`,
+  },
+  {
+    name: 'pizza',
+    image: `/images/pizza.png`,
+  },
+  {
+    name: 'pasta',
+    image: `/images/pasta.png`,
+  },
+  {
+    name: 'steak',
+    image: `/images/steak.png`,
+  },
+  {
+    name: 'cake',
+    image: `/images/cake.png`,
+  },
+  {
+    name: 'smoothies',
+    image: `/images/smoothies.png`,
+  },
+]
