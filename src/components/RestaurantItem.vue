b0b3c58af0b6fe78a0c7a58052023af9d09862da
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-06-19 21:59:45 +0200

    Styles for Restaurant list

--- src/components/RestaurantItem.vue
+++ src/components/RestaurantItem.vue
@@ -10,13 +10,53 @@ const goToRestaurant = (id: string) => {
 </script>
 
 <template>
-  <div @click="goToRestaurant(restaurant.id)">
-    <p>{{ restaurant.name }}</p>
+  <a class="restaurant-wrapper" @click="goToRestaurant(restaurant.id)">
     <img :src="restaurant.image_url" />
-    <p>{{ restaurant.rating }}</p>
-    <p>{{ restaurant.price }}</p>
+    <div class="restaurant__info--container">
+      <p class="restaurant__name">{{ restaurant.name }}</p>
+      <div class="restaurant__info">
+        <p class="restaurant__rating">{{ restaurant.rating }}</p>
+        <p class="restaurant__price">{{ restaurant.price }}</p>
+      </div>
+    </div>
+
     <hr />
-  </div>
+  </a>
 </template>
 
-<style scoped></style>
+<style scoped>
+.restaurant-wrapper {
+  background-color: #fff;
+  height: 100px;
+  display: flex;
+  flex-direction: row;
+  align-self: stretch;
+  align-items: center;
+  border-radius: 50px;
+  margin-block: 10px;
+}
+img {
+  width: 75px;
+  height: 75px;
+  border-radius: 50px;
+  margin-inline: 10px;
+}
+
+.restaurant__info--container {
+  flex: 1;
+}
+.restaurant__info {
+  display: flex;
+}
+.restaurant__name {
+  font-size: 1.1rem;
+  font-weight: bold;
+  margin-bottom: 1em;
+}
+.restaurant__rating {
+  margin-right: 20px;
+}
+.restaurant__price {
+  color: gold;
+}
+</style>
