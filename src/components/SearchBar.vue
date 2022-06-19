ba56d21b86bcd5462d2adc7573c461995d0db946
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-06-19 16:17:31 +0200

    SearchBar component: add basic behaviour (and maybe the final one, not sure if anything else is actually needed)

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const emit = defineEmits<{
+  (e: 'searchTerm', text: string): void
+}>()
+
+const searchField = ref('')
+function searchText() {
+  emit('searchTerm', searchField.value)
+}
+</script>
 <template>
   <div>
-    <input type="search" name="restaurantType" id="restaurantType" />
+    <input
+      type="search"
+      name="restaurantType"
+      id="restaurantType"
+      v-model="searchField"
+      @input="searchText" />
   </div>
 </template>
 <style scoped></style>
