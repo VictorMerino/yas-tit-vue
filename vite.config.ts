547c7b52bb7a7993cfa01731c6980a549cb834af
Victor Merino <5514514+VictorMerino@users.noreply.github.com>
2022-06-28 20:02:49 +0200

    Add needed tests config to vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { fileURLToPath, URL } from 'url'
 
 import { defineConfig } from 'vite'
@@ -8,7 +9,11 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  },
 })
